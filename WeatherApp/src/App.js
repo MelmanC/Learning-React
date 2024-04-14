e14421fd841b2218308b930d3f1b142d535b47e1
Nolann Dubos <nolann.dubos@epitech.eu>
2024-04-14 19:25:27 +0200

    [ADD] Push WeatherApp

---
+++ WeatherApp/src/App.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const handleWeather = async (event) => {
+    event.preventDefault();
+    const apiKey = '65a7553024ad1a9a67f43f4b46f99993';
+
+    try {
+      const response  = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+      const data = await response.json();
+      console.log(data);
+      setWeather(data);
+    } catch (error) {
+      console.error(error);
+    }
+
+  };
+
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <form onSubmit={handleWeather}>
+        <input type="text" value={city} onChange={(event) => setCity(event.target.value)} />
+        <button type='submit'>Get Weather</button>
+      </form>
+      <br/>
+      <br/>
+      <h2>{weather.name} {weather.sys && weather.sys.country}</h2>
+      <h3>{weather.weather && weather.weather[0].description}</h3>
+      <h3>{weather.main && weather.main.temp} {weather.name ? "°C" : ""}</h3>
+    </div>
+  );
+}
+
+export default App;
